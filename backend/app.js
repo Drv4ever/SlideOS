778c4dffba6d7ce88033a39101c4b4b932bc44b0
Drv4ever <dhruvjain632@gmail.com>
2026-04-04 19:04:12 +0530

    added proper cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://slide-os.vercel.app"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
